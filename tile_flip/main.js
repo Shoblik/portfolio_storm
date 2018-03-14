974aa4391cc26481c9fcdad5185d7e2535cfc5c8
Shoblik <shoblik@yahoo.com>
2018-03-13 22:12:45 -0700

    fixed tile flipping bug

--- tile_flip/main.js
+++ tile_flip/main.js
@@ -23,17 +23,17 @@ function leftToRightFlip() {
             tileHeight = tileHeight.slice(0, i);
         }
     }
-    let rowMultiplier = Math.ceil(screenWidth / tileWidth);
+    let rowMultiplier = Math.ceil(screenHeight / tileWidth);
 
     //column iterator
     let timer = 0;
-    for (let column = 0; column < columnMultiplier; column++) {
-        for (let row = 0; row < rowMultiplier; row++) {
+    for (let row = 0; row < rowMultiplier; row++) {
+        for (let column = 0; column < columnMultiplier; column++) {
             setTimeout(function() {
                     $($('.flipper')[row * 40 + column]).css('transform', 'rotateY(180deg)');
                 }, timer);
-                timer += 200;
-            if (row + 1 === rowMultiplier) {
+                timer += 5;
+            if (column + 1 === columnMultiplier) {
                 timer = 0;
             }
         }
